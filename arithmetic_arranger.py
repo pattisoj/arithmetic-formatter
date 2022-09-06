db461ba0574587cfbefd1451b679468bf3ea5d67
pattisoj <joshpattison23@icloud.com>
2022-09-06 10:51:35 +0100

    Passing 4/10 tests - Error Checking Complete

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,24 @@
+from optparse import OptParseError
+
+
 def arithmetic_arranger(problems):
+# First handle the possible errors
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+    for problem in problems:
+        if "*" in problem or "/" in problem:
+            return "Error: Operator must be '+' or '-'."
+        
+        operand_one = problem.split()[0]
+        operand_two = problem.split()[2]
 
-    return arranged_problems
+        if operand_one.isnumeric() == False or operand_two.isnumeric() == False:
+            return "Error: Numbers must only contain digits."
+
+        if len(operand_one) > 4 or len(operand_two) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        
+        
+
+    return "Error Checking"
